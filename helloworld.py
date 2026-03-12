6113ef5442a344b9f4233cb256b127227e30be24
shivasaidhattareddykthestackly <shivasaidhattareddyk@thestackly.com>
2026-03-12 08:53:19 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,63 @@
+print("Hello","World","Welcome","Python")
+print("Laptop","Mouse","Keyboard",sep=" | ")
+
+name="Ravi"
+age=22
+city="Chennai"
+print(name,age,city,sep="-")
+
+name,age,student="Meena",20,True
+print(name,age,student)
+
+word="Python"
+print(word[0])
+print(word[2])
+print(word[-1])
+
+print(25+10)
+print(50-20)
+print(8*5)
+print(100/10)
+print(10%3)
+print(2**4)
+print(20//3)
+
+print(3+2*5**2)
+
+num=50
+num+=25
+print(num)
+
+num=100
+num/=10
+print(num)
+
+print(10>5)
+print(20<15)
+print(5==5)
+print(10!=8)
+print(7>=7)
+print(6<=2)
+
+a="apple"
+b="Apple"
+print(a==b)
+
+print(10>5 and 5==5)
+print(5>10 or 10==10)
+print(not(5>2))
+
+numbers=[10,20,30,40,50]
+print(20 in numbers)
+print(60 in numbers)
+print(30 not in numbers)
+
+a=10
+b=20
+a,b=b,a
+print("a =",a)
+print("b =",b)
+
+a=6
+b=3
+print(a^b)
